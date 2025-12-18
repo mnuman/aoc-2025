01a00cd3881258a0b4a478842d41271ffbea8422
Milco Numan <milco.numan@gmail.com>
2025-12-18 14:49:56 +0000

    6-2 reducing my way to the bottom!

--- solutions/day06.py
+++ solutions/day06.py
@@ -11,6 +11,28 @@ def read_file(fname):
     return data, operations
 
 
+def read_file_part2(fname):
+    all_lines = fu.read_raw_file(fname).splitlines()
+    data_lines, operation_lines = all_lines[:-1], all_lines[-1]
+    ops = [c for c in operation_lines if c != " "]
+    col = 0
+    parsed = []
+    operations = []
+    for op in ops:
+        assert op in ("*", "+")
+        operands = []
+        while col < len(data_lines[0]):
+            operand = "".join(data_lines[row][col] for row in range(len(data_lines)))
+            col += 1
+            if trim(operand):
+                operands.append(int(trim(operand)))
+            else:
+                break
+        parsed.append(operands)
+        operations.append(mul if op == "*" else add)
+    return operations, parsed
+
+
 def trim(s):
     return s.lstrip().rstrip()
 
@@ -25,6 +47,15 @@ def part1(fname):
     return result
 
 
+def part2(fname):
+    operations, operands = read_file_part2(fname)
+    result = 0
+    for idx, operation in enumerate(operations):
+        result += reduce(operation, operands[idx])
+    return result
+
+
 if __name__ == "__main__":
     fname = "day06.txt"
     print("Part 1:", part1(fname))
+    print("Part 2:", part2(fname))
--- tests/test_day06.py
+++ tests/test_day06.py
@@ -1,3 +1,4 @@
+from operator import add, mul
 import day06
 
 
@@ -10,3 +11,15 @@ def test_read_file():
 def test_part1():
     result = day06.part1("test_day06.txt")
     assert result == 4277556
+
+
+def test_read_file_part2():
+    ops, operands = day06.read_file_part2("test_day06.txt")
+    assert ops == [mul, add, mul, add]
+    assert len(operands) == len(ops)
+    assert operands[0] == [1, 24, 356]
+
+
+def test_part2():
+    result = day06.part2("test_day06.txt")
+    assert result == 3263827
